cfa2668c181c865ac17e7612558b8d68f43f4ca2
s220035631 <s220035631@seu.edu.sa>
2026-04-21 13:42:05 +0300

    fix path initialization

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
-
 const express = require('express');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '..')));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
