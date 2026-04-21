2884f3aaf7cff20a3b94a540bf38fb4ffd9563d9
s220035631 <s220035631@seu.edu.sa>
2026-04-21 13:26:06 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,11 @@ app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'crime_system'
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'crime_system'
 });
 
 db.connect((err) => {
